48c016a48f48a50b2e35671d8a0b0aea281f057d
enuchsa <enuchsa@gmail.com>
2023-12-28 23:01:28 -0300

    feat: Adicionando url group e view group detail

--- heroes/group/urls.py
+++ heroes/group/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from heroes.group.views import GroupList
+from rest_framework.urlpatterns import format_suffix_patterns
+
+from heroes.group.views import GroupList, GroupDetail
 
 urlpatterns = [
     path('groups/', GroupList.as_view()),
+    path('group/<int:pk>/', GroupDetail.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
